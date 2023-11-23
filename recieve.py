a771a37f37ba01f09c75010a5b42f687ee351de3
Jonatan Kasperczak <jakasperczak@gmail.com>
2023-11-23 22:15:17 +0100

    v0 dziala

---
+++ recieve.py
@@ -0,0 +1,62 @@
+import scapy.all
+import json
+
+with open('vars.json', 'r') as f:
+    code = json.load(f)
+o = code['O']
+d = code['D']
+i = code['I']
+j = code['J']
+print(type(o))
+print(o, d, i, j)
+
+stegand_cap = scapy.all.rdpcap('stegand.pcap')
+stegand_packets = [pac for pac in stegand_cap if pac.haslayer(scapy.all.TCP)]
+steg = [pac for pac in stegand_packets if pac.haslayer(scapy.all.Ether) and pac.dst.lower() == "ac:19:8e:c7:60:a0"]
+print(len(steg))
+steg.sort()
+
+seqNrs = []
+seqAll = []
+seqRet = []
+for packet in steg:
+    seqAll.append(packet.seq)
+    if packet.seq not in seqNrs:
+        seqNrs.append(packet.seq)
+    elif packet.seq not in seqRet:
+        seqRet.append(packet.seq)
+
+print(len(seqNrs))
+print(len(seqRet))
+seqNrs.sort()
+
+binary_rep = []
+
+# retransmited.append(seqNrs[O+D*i])
+# retransmited.append(seqNrs[O+D*i+I])
+# retransmited.append(seqNrs[O+D*i+I+J])
+# '0', '1', '1', '0', '0', '0', '0', '1', '0', '1', '1', '0', '1', '1', '0', '1', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '1', '1', '0', '0', '1', '0', '0', '1', '1', '1', '1', '0', '0', '1', '0', '1', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '0', '0', '0', '1'
+
+#133865316
+print(seqNrs[1*d+o])
+if seqNrs[1*d+o] in seqRet:
+    print(1)
+for u in range(len(seqNrs) // d-1):
+    x = 0
+    if seqNrs[u*d+o] in seqRet:
+        x += 1
+    if seqNrs[u*d+o+i] in seqRet:
+        x += 1
+    if seqNrs[u*d+o+i+j] in seqRet:
+        x += 1
+    if x == 3:
+        binary_rep.append('1')
+    else:
+        binary_rep.append('0')
+           
+def decode(binary_representation):
+    decode_hidden_message = lambda binary_str: ''.join(chr(int(''.join(binary_str[i:i+8]), 2)) for i in range(0, len(binary_str), 8))
+    decoded_rep = decode_hidden_message(binary_representation)
+    return decoded_rep
+print(binary_rep)
+print(decode(binary_rep))
---
+++ send.py
@@ -0,0 +1,77 @@
+import scapy.all
+from collections import Counter
+from random import randint
+import json
+
+## Step 1
+hidden_message = "nie_ma_zla"
+encode_hidden_message = lambda x: [z for y in x for z in f'{ord(y):08b}' ]
+binary_representation = encode_hidden_message(hidden_message)
+print(binary_representation)
+
+## Step 2
+scapy_cap = scapy.all.rdpcap('webowyruch.pcap')
+htc_packets = [pac for pac in scapy_cap if pac.haslayer(scapy.all.TCP)]
+inbound = [pac for pac in htc_packets if pac.haslayer(scapy.all.Ether) and pac.dst.lower() == "ac:19:8e:c7:60:a0"]
+# ac:19:8e:c7:60:a0
+
+inbound.sort()
+print(len(inbound))
+seq_all = []
+seqNrs = []
+seq2= []
+for packet in inbound:
+    seq_all.append(packet.seq)
+    if packet.seq not in seqNrs:
+        seqNrs.append(packet.seq)
+
+seqNrs.sort()
+print(len(seqNrs))  
+
+D = randint(20, len(seqNrs) // (len(binary_representation)+1))
+O = randint(1, D)
+I = randint(2, D)
+J = randint(1, D-I)
+varsy = { 
+    "D":D,
+    "O":O,
+    "I":I,
+    "J":J
+    }
+
+jsonobject = json.dumps(varsy)
+with open('vars.json', 'w') as file:
+    file.write(jsonobject)
+#print(jsonobject)
+# Step 3 -> check if we need to sen 1 or 0, if 1 add seq numbers to the list.
+
+retransmited = []
+for i in range(len(binary_representation)):
+    if binary_representation[i] == '1':
+        retransmited.append(seqNrs[O+D*i])
+        retransmited.append(seqNrs[O+D*i+I])
+        retransmited.append(seqNrs[O+D*i+I+J])
+print(len(retransmited))
+print(retransmited[0])
+
+
+if all(count == 1 for count in Counter(retransmited).values()):
+    print("List contains all unique elements")
+else:
+    print("List contains does not contains all unique elements")
+
+
+outbound = []
+for i in inbound:
+    outbound.append(i)
+    if i.seq in retransmited:
+        outbound.append(i)
+print(len(outbound))
+
+print(len(inbound))
+
+def write(pkt):
+    scapy.all.wrpcap('stegand.pcap', pkt, append=True)  #appends packet to output file
+
+write(outbound)
+
